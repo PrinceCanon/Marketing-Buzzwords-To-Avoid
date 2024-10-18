cd49517f1d0bbdfdefb7f46b3bfbb3c984785f8e
PrinceCanon <egumotong@gmail.com>
2024-10-18 19:45:37 -0400

    updated file flow

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+// import ReactDOM from "react-dom";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./../public/styles.css";
+import App from "./App";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create a root
+root.render(<App />);
